de910dfd8adad9efdf6718c8c461e76b30204cef
Shawn Petros <spetros@amfam.com>
2021-08-25 09:20:18 -0500

    stuff

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import {useState} from 'react';
+import { Col, Container, Div, Input, Row } from 'atomize'
+import './App.css';
+
+function App() {
+  const [vals, setVals] = useState({
+    price: '',
+    down: '',
+    rate: '',
+    term: '',
+    cc: '',
+  })
+  const amort = (pv, n, r) => {
+    const monthlyRate = r / 12
+    const payment = pv * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -n)));
+
+    let balance = pv;
+    const amortTable = [];
+    /**
+     * Loop that calculates the monthly Loan amortization amounts then adds
+     * them to the return string
+     */
+    for (let count = 0; count < n; ++count) {
+      // collect the data
+      const tableRow = {
+        month:0,
+        balance:'',
+        interest:'',
+        monthlyPrincipal: '',
+      };
+
+      // in-loop interest amount holder
+      let interest = 0;
+
+      // in-loop monthly principal amount holder
+      let monthlyPrincipal = 0;
+
+      // add month number in col 1 using the loop count variable
+      tableRow.month = (count + 1);
+
+      // code for displaying in loop balance
+      tableRow.balance = balance.toFixed(2);
+
+      // calc the in-loop interest amount and display
+      interest = balance * monthlyRate;
+      tableRow.interest = interest.toFixed(2);
+
+      // calc the in-loop monthly principal and display
+      monthlyPrincipal = payment - interest;
+      tableRow.monthlyPrincipal = monthlyPrincipal.toFixed(2);
+
+      amortTable.push(tableRow);
+
+      //update the balance for each loop iteration
+      balance = balance - monthlyPrincipal;
+    }
+
+    return { payment: payment.toFixed(2), amortTable }
+  }
+  const parseInputs = (values) => {
+    const {rate, price, term} = values;
+    const parsedPrice = parseFloat(price)
+    const parsedRate = parseFloat(rate)/100.0
+    const parsedTerm = parseInt(term)*12
+    return {
+      parsedTerm,
+      parsedRate,
+      parsedPrice,
+    }
+  }
+  const validateInputs = ([, val]) => !(val === null || val === '')
+  const isTrue = (val) => val === true
+  const handleChange = e => setVals({...vals,[e.target.name]: e.target.value})
+  const calculate = () => {
+    const validInputs = Object.entries(vals)
+      .map(validateInputs)
+      .some(isTrue)
+    if (validInputs) {
+      // do stuff
+      const {parsedPrice, parsedTerm, parsedRate} = parseInputs(vals)
+      const {payment, amortTable} = amort(parsedPrice, parsedTerm, parsedRate);
+      console.log({parsedPrice, parsedRate, parsedTerm, payment, amortTable})
+    } else {
+      return false
+    }
+  }
+  return (
+    <Container className="App">
+    <Div>
+      <header className="App-header">
+        <pre>{JSON.stringify(vals, null, 2)}</pre>
+        <Row>
+          <Col>
+          <label>Sale Price: </label></Col><Col><Input h="2rem" name="price" value={vals.price} onChange={handleChange} /></Col>
+
+        </Row>
+
+        <label>Downpayment %: <Input h="2rem" name="down" value={vals.down} onChange={handleChange} /></label>
+        <label>Interest %: <Input h="2rem" name="rate" value={vals.rate} onChange={handleChange} /></label>
+        <label>Term (yrs): <Input h="2rem" name="term" value={vals.term} onChange={handleChange} /></label>
+        <label>Closing Costs: <Input h="2rem" name="cc" value={vals.cc} onChange={handleChange} /></label>
+        <button onClick={calculate}>Calculate</button>
+      </header>
+    </Div>
+    </Container>
+  );
+}
+
+export default App;
